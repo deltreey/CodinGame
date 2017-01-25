a5c955c6eb68e4374a273ed0da21b15672677f95
deltreey <suicidolt@gmail.com>
2017-01-25 18:02:15 -0500

    let's do rust

---
+++ rust/stock_exchange_losses.rs
@@ -0,0 +1,34 @@
+use std::io;
+
+macro_rules! print_err {
+    ($($arg:tt)*) => (
+        {
+            use std::io::Write;
+            writeln!(&mut ::std::io::stderr(), $($arg)*).ok();
+        }
+    )
+}
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+fn main() {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let n = parse_input!(input_line, i32);
+    let mut inputs = String::new();
+    io::stdin().read_line(&mut inputs).unwrap();
+    for i in inputs.split(" ") {
+        let v = parse_input!(i, i32);
+    }
+
+    // Write an action using println!("message...");
+    // To debug: print_err!("Debug message...");
+
+    println!("answer");
+}
